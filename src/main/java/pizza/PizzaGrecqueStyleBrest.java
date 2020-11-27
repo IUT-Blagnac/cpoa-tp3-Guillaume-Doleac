b903f96f3b8e02e42f803fead94e910f190ec9fe
guillaume.doleac <guigui.doleac@outlook.fr>
2020-11-27 19:33:36 +0100

    Depot tp3

---
+++ src/main/java/pizza/PizzaGrecqueStyleBrest.java
@@ -0,0 +1,33 @@
+package pizza;
+/**
+ * @author bruel (from O'Reilly Head-First series)
+ * @depend - * - Pizza
+ */
+public class PizzaGrecqueStyleBrest extends Pizza {
+
+	public PizzaGrecqueStyleBrest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void bake() {
+		
+	}
+
+	@Override
+	public void cut() {
+	}
+
+	@Override
+	public void wrap() {
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return super.getName();
+	}
+
+	
+}
